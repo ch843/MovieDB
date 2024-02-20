8ea0891495bb00ad98389a01170bf878dc7c5dad
ch843 <cassidy.hardisty@gmail.com>
2024-02-20 10:14:05 -0700

    updated DB

--- MovieDB/Models/Movie.cs
+++ MovieDB/Models/Movie.cs
@@ -14,8 +14,8 @@ public class Movie
     public string Director { get; set; }
     public string Rating { get; set; }
     
-    public bool Edited { get; set; }
-    public string Lent { get; set; }
+    public bool? Edited { get; set; }
+    public string? Lent { get; set; }
     
-    public string Notes { get; set; }
+    public string? Notes { get; set; }
 }
