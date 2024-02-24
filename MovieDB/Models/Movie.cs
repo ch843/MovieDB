06f2b57438639ce2616c903e9d1c4070d19663ce
ch843 <cassidy.hardisty@gmail.com>
2024-02-23 20:57:02 -0700

    Finished reqs

--- MovieDB/Models/Movie.cs
+++ MovieDB/Models/Movie.cs
@@ -8,18 +8,19 @@ public class Movie
     [Required]
     public int MovieId { get; set; }
     [ForeignKey("CategoryId")]
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
     public Category Category { get; set; }
     [Required]
     public string Title { get; set; }
     [Required]
+    [Range(1888, int.MaxValue, ErrorMessage = "The year must be greater than 1888")]
     public int Year { get; set; }
-    public string Director { get; set; }
-    public string Rating { get; set; }
+    public string? Director { get; set; }
+    public string? Rating { get; set; }
     [Required]
     
-    public int? Edited { get; set; }
-    public string? Lent { get; set; }
+    public int Edited { get; set; }
+    public string? LentTo { get; set; }
     [Required]
     public int CopiedToPlex { get; set; }
     public string? Notes { get; set; }
